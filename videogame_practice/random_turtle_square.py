9d08baa8c4d68ae41043e26e04cf4a14c776cacf
MarwanBit <38559602+MarwanBit@users.noreply.github.com>
2019-01-14 23:23:30 -0500

    Update random_turtle_square.py

--- videogame_practice/random_turtle_square.py
+++ videogame_practice/random_turtle_square.py
@@ -2,6 +2,7 @@ import turtle as turtle
 import random as rand
 import hex_color_generator as hex_gen
 
+#defines objects 
 turtle_god = turtle.Turtle()
 screen = turtle.Screen()
 
@@ -12,10 +13,12 @@ main_loop = True
 global_color_variable = 0
 
 while main_loop == True:
+
+    #Square
     for i in range (0,4):
         turtle_god.forward(30)
         turtle_god.right(90)
-        
+    #Repositions to a random area and changes the color    
     turtle_god.penup()
     turtle_god.goto(rand.randint(-200, 200), rand.randint(-200, 200))
     turtle_god.pendown()
